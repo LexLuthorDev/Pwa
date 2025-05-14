5ee9dba9b682b697bade671884fe77666b325d91
Lexluthor <lexluthordevfull@gmail.com>
2025-05-13 23:52:26 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "frontend",
+        short_name: "frontend",
+        description: "descrição",
+        theme_color: "#2da1c4",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "favicon.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
