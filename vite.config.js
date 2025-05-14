04c80872b932dcb25f34ef1ebd33a655240b3b5e
Lexluthor <lexluthordevfull@gmail.com>
2025-05-14 00:16:46 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /*server: {
+    allowedHosts: [
+      "d44d-45-160-89-106.ngrok-free.app", // coloque seu host atual do ngrok aqui
+    ],
+  },*/
   plugins: [
     react(),
     VitePWA({
