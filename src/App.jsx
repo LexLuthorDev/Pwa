5abe979eba93ad7f9a87557b56308914b0b90531
Lexluthor <lexluthordevfull@gmail.com>
2025-06-03 11:56:45 -0300

    ok favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useState, useRef, useEffect } from "react";
+
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import PrivateRoute from "./routes/PrivateRoute";
+
 import { AuthProvider } from "./context/AuthContext";
 import { DadosJogadorProvider } from "./context/DadosJogadorContext";
 
@@ -16,8 +18,29 @@ import MinesGame from "./pages/games/mines/page";
 import Login from "./pages/login/page";
 import Cadastro from "./pages/cadastro/page";
 
+import { useTheme } from "./context/ThemeContext";
+
 // ==================== COMPONENTE APP PRINCIPAL ====================
 function App() {
+  const theme = useTheme();
+  useEffect(() => {
+    const iconLink = document.querySelector("link[rel='icon']");
+    if (iconLink && theme?.faviconUrl) {
+      iconLink.href = theme.faviconUrl;
+    }
+
+    const appleTouchIcon = document.querySelector(
+      "link[rel='apple-touch-icon']"
+    );
+    if (appleTouchIcon && theme?.faviconUrl) {
+      appleTouchIcon.href = theme.faviconUrl;
+    }
+
+    const maskIcon = document.querySelector("link[rel='mask-icon']");
+    if (maskIcon && theme?.faviconUrl) {
+      maskIcon.href = theme.faviconUrl;
+    }
+  }, [theme?.faviconUrl]);
   return (
     <>
       <AuthProvider>
@@ -31,7 +54,14 @@ function App() {
                 </DadosJogadorProvider>
               }
             />
-            <Route path="/promocoes" element={<DadosJogadorProvider><PagePromocoes /> </DadosJogadorProvider>} />
+            <Route
+              path="/promocoes"
+              element={
+                <DadosJogadorProvider>
+                  <PagePromocoes />{" "}
+                </DadosJogadorProvider>
+              }
+            />
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route
