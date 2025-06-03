bbcaa9bf3668f83d87512183491bbc441f3a8867
Lexluthor <lexluthordevfull@gmail.com>
2025-06-03 04:55:05 -0300

    ok

--- src/api/jogador.js
+++ src/api/jogador.js
@@ -5,5 +5,5 @@ export const temaCassino = () => http.post("/cassino/tema");
 
 export const createJogador = (data) => http.post("/jogador", data);
 export const loginJogador = (data) => http.post("/jogador/login", data);
-export const getJogadorMe = () => http.get("/jogador/me");
+export const getJogadorMe = () => http.post("/jogador/me");
 
